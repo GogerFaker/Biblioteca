b23aaefa25aa9ec1abc0cc51be201dba89423a74
Victor Gomes <victorjps.etec@gmail.com>
2019-09-02 22:49:52 -0300

    Criação do login

--- app/src/main/java/com/example/biblioteca/MainActivity.kt
+++ app/src/main/java/com/example/biblioteca/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.biblioteca
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart(){
+        super.onStart()
+
+        var intent = Intent(this, LoginActivity::class.java)
+        startActivityForResult(intent, 1010)
     }
 }
